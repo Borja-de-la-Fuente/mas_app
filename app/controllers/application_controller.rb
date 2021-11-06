364a709e6ba75fb5697291c585534fa972f2228b
Borja de la Fuente <bdelafuente@epages.com>
2021-11-06 13:25:56 +0100

    Add own parametters in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "hello, world!"
+    
+    before_action :authenticate_user!
+    before_action :configure_permitted_parametters, if: :devise_controller?
+    
+    protected
+    # Permite nuevos parametros en Devise
+    def configure_permitted_parametters
+        attributes = [:name]
+        devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+        devise_parameter_sanitizer.permit(:account_update, keys: attributes)
     end
 end
